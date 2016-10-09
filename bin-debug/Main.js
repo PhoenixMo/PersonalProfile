a340fcfd1e6c0056e5983449086526681f1fc48d
PhoenixMo <1002375424@qq.com>
2016-10-09 14:58:10 +0800

    音乐模块施工中......

--- bin-debug/Main.js
+++ bin-debug/Main.js
@@ -41,37 +41,189 @@ var Poke = (function (_super) {
         this.stage.addEventListener(egret.TouchEvent.TOUCH_MOVE, this.mouseMove, this);
     };
     p.mouseMove = function (evt) {
+        var _this = this;
         if (this._touchStatus) {
             this.y = evt.stageY - this._distance.y;
+        }
+        if (this.y < -1136 / 3) {
+            egret.Tween.get(this).to({ x: 4 / 5 * this.stageW, y: -1136 }, 400, egret.Ease.sineIn)
+                .call(function () { _this.parent.addChildAt(_this, 1); }).to({ x: 4 / 5 * this.stageW, y: 0 }, 100, egret.Ease.sineIn);
+            this.stage.removeEventListener(egret.TouchEvent.TOUCH_MOVE, this.mouseMove, this);
+        }
+        else if (this.y < 0) {
+            egret.Tween.get(this).to({ x: 4 / 5 * this.stageW, y: 0 }, 200, egret.Ease.sineIn)
+                .to({ x: 4 / 5 * this.stageW, y: 25 }, 100, egret.Ease.sineIn).to({ x: 4 / 5 * this.stageW, y: 0 }, 100, egret.Ease.sineIn);
+        }
+        if (this.y > 1136 / 3) {
+            egret.Tween.get(this).to({ x: 4 / 5 * this.stageW, y: 1136 }, 400, egret.Ease.sineIn)
+                .call(function () { _this.parent.addChildAt(_this, 1); }).to({ x: 4 / 5 * this.stageW, y: 0 }, 100, egret.Ease.sineIn);
+            this.stage.removeEventListener(egret.TouchEvent.TOUCH_MOVE, this.mouseMove, this);
+        }
+        else if (this.y > 0) {
+            egret.Tween.get(this).to({ x: 4 / 5 * this.stageW, y: 0 }, 200, egret.Ease.sineIn)
+                .to({ x: 4 / 5 * this.stageW, y: -25 }, 100, egret.Ease.sineIn).to({ x: 4 / 5 * this.stageW, y: 0 }, 100, egret.Ease.sineIn);
         }
     };
     p.mouseUp = function (evt) {
         this._touchStatus = false;
-        if (this.y < -1136 / 3) {
-            egret.Tween.get(this).to({ x: 4 / 5 * this.stageW, y: -1136 }, 200, egret.Ease.sineIn)
-                .wait(300).to({ x: 4 / 5 * this.stageW, y: 0 }, 100, egret.Ease.sineIn);
-            this.parent.addChildAt(this, 1);
-            this.stage.removeEventListener(egret.TouchEvent.TOUCH_MOVE, this.mouseMove, this);
-        }
-        else {
-            egret.Tween.get(this).to({ x: 4 / 5 * this.stageW, y: 0 }, 200, egret.Ease.sineIn)
-                .to({ x: 4 / 5 * this.stageW, y: 20 }, 100, egret.Ease.sineIn).to({ x: 4 / 5 * this.stageW, y: 0 }, 100, egret.Ease.sineIn);
-        }
-        if (this.y > 1136 / 3) {
-            egret.Tween.get(this).to({ x: 4 / 5 * this.stageW, y: -1136 }, 200, egret.Ease.sineIn)
-                .wait(300).to({ x: 4 / 5 * this.stageW, y: 0 }, 100, egret.Ease.sineIn);
-            this.parent.addChildAt(this, 1);
-            this.stage.removeEventListener(egret.TouchEvent.TOUCH_MOVE, this.mouseMove, this);
-        }
-        else {
-            egret.Tween.get(this).to({ x: 4 / 5 * this.stageW, y: 0 }, 200, egret.Ease.sineIn)
-                .to({ x: 4 / 5 * this.stageW, y: -25 }, 100, egret.Ease.sineIn).to({ x: 4 / 5 * this.stageW, y: 0 }, 100, egret.Ease.sineIn);
-        }
         this.stage.removeEventListener(egret.TouchEvent.TOUCH_MOVE, this.mouseMove, this);
     };
     return Poke;
 }(egret.DisplayObjectContainer));
 egret.registerClass(Poke,'Poke');
+var Music = (function (_super) {
+    __extends(Music, _super);
+    function Music() {
+        _super.call(this);
+        this._pauseTime = 30;
+        this.addEventListener(egret.Event.ADDED_TO_STAGE, this.onAddToStage, this);
+    }
+    var d = __define,c=Music,p=c.prototype;
+    p.onAddToStage = function (event) {
+        this.loadSound();
+    };
+    /*** 本示例关键代码段开始 ***/
+    //加载
+    p.loadSound = function () {
+        var sound = this._sound = new egret.Sound();
+        ;
+        //sound 加载完成监听
+        sound.addEventListener(egret.Event.COMPLETE, function (e) {
+            this.init();
+        }, this);
+        sound.load("resource/assets/ccnn.mp3");
+    };
+    //播放
+    p.play = function () {
+        //sound 播放会返回一个 SoundChannel 对象，暂停、音量等操作请控制此对象
+        this._channel = this._sound.play(this._pauseTime, 1);
+        this._channel.addEventListener(egret.Event.SOUND_COMPLETE, this.onComplete, this);
+        this.addEventListener(egret.Event.ENTER_FRAME, this.onTimeUpdate, this);
+    };
+    //停止
+    p.stop = function () {
+        if (this._channel) {
+            this._channel.removeEventListener(egret.Event.SOUND_COMPLETE, this.onComplete, this);
+            this.removeEventListener(egret.Event.ENTER_FRAME, this.onTimeUpdate, this);
+            this._channel.stop();
+            this._channel = null;
+        }
+    };
+    //播放完成
+    p.onComplete = function (e) {
+        console.log("播放完成");
+        this.stop();
+        this.setAllAbled(false);
+        this.setProgress(0);
+    };
+    //更新进度
+    p.onTimeUpdate = function (e) {
+        var position = this._channel ? this._channel.position : 0;
+        this.setProgress(position);
+    };
+    p.setProgress = function (position) {
+        this._updateTxt.text = position.toFixed(1) + "/" + this._sound.length.toFixed(1);
+        var w = (position / this._sound.length) * 400;
+        this._bar.x = w + this.stage.stageWidth / 2 - 200;
+        var mask = this._progress.mask || new egret.Rectangle(0, 0, 0, 60);
+        mask.x = w;
+        mask.width = 400 - w;
+        this._progress.mask = mask;
+    };
+    p.init = function () {
+        var rap = 180;
+        var rapH = 200;
+        //play
+        var playTxt = this._playTxt = new egret.TextField();
+        playTxt.text = "播放";
+        playTxt.size = 60;
+        playTxt.x = 80;
+        playTxt.y = 200 + rapH;
+        playTxt.touchEnabled = true;
+        playTxt.addEventListener(egret.TouchEvent.TOUCH_TAP, function () {
+            this.play();
+            this.setAllAbled(true);
+        }, this);
+        this.addChild(playTxt);
+        //stop
+        var stopTxt = this._stopTxt = new egret.TextField();
+        stopTxt.text = "停止";
+        stopTxt.size = 60;
+        stopTxt.x = playTxt.x + rap * 1;
+        stopTxt.y = 200 + rapH;
+        stopTxt.touchEnabled = true;
+        stopTxt.addEventListener(egret.TouchEvent.TOUCH_TAP, function () {
+            if (this._channel) {
+                this._pauseTime = 0;
+                this.stop();
+                this.onTimeUpdate();
+            }
+            this.setAllAbled(false);
+        }, this);
+        this.addChild(stopTxt);
+        //pause 
+        var pauseTxt = this._pauseTxt = new egret.TextField();
+        pauseTxt.text = "暂停";
+        pauseTxt.size = 60;
+        pauseTxt.x = playTxt.x + rap * 2;
+        pauseTxt.y = 200 + rapH;
+        pauseTxt.touchEnabled = true;
+        pauseTxt.addEventListener(egret.TouchEvent.TOUCH_TAP, function () {
+            if (this._channel) {
+                this._pauseTime = this._channel.position;
+                this.stop();
+            }
+            this.setAllAbled(false);
+        }, this);
+        this.addChild(pauseTxt);
+        this.setAllAbled(false);
+        var bg = new egret.Shape();
+        this.addChild(bg);
+        bg.x = this.stage.stageWidth / 2 - 200;
+        bg.y = 100 - 5 + rapH;
+        bg.graphics.beginFill(0x999999);
+        bg.graphics.drawRoundRect(0, 0, 400, 10, 5, 5);
+        bg.graphics.endFill();
+        this._progress = new egret.Shape();
+        this.addChild(this._progress);
+        this._progress.x = this.stage.stageWidth / 2 - 200;
+        this._progress.y = 100 - 5 + rapH;
+        this._progress.graphics.beginFill(0xffff00);
+        this._progress.graphics.drawRoundRect(0, 0, 400, 10, 5, 5);
+        this._progress.graphics.endFill();
+        this._bar = new egret.Shape();
+        this.addChild(this._bar);
+        this._bar.x = this.stage.stageWidth / 2 - 200;
+        this._bar.y = 100 + rapH;
+        this._bar.graphics.beginFill(0xffff00);
+        this._bar.graphics.drawCircle(0, 0, 20);
+        this._bar.graphics.endFill();
+        this._updateTxt = new egret.TextField();
+        this._updateTxt.text = 0 + "/" + this._sound.length.toFixed(1);
+        this._updateTxt.width = 200;
+        this._updateTxt.size = 30;
+        this._updateTxt.x = this.stage.stageWidth / 2 - 100;
+        this._updateTxt.y = 50 + rapH;
+        this._updateTxt.textAlign = egret.HorizontalAlign.CENTER;
+        this.addChild(this._updateTxt);
+    };
+    p.setAllAbled = function (isPlaying) {
+        this.setTextAbled(this._playTxt, !isPlaying);
+        this.setTextAbled(this._stopTxt, isPlaying);
+        this.setTextAbled(this._pauseTxt, isPlaying);
+    };
+    p.setTextAbled = function (text, touchEnabled) {
+        text.touchEnabled = touchEnabled;
+        if (touchEnabled) {
+            text.textColor = 0xffffff;
+        }
+        else {
+            text.textColor = 0x999999;
+        }
+    };
+    return Music;
+}(egret.DisplayObjectContainer));
+egret.registerClass(Music,'Music');
 var Main = (function (_super) {
     __extends(Main, _super);
     function Main() {
@@ -226,6 +378,8 @@ var Main = (function (_super) {
         this.zbhd(p3);
         p3.x = 4 / 5 * stageW;
         p3.y = 0;
+        var mus = new Music;
+        this.addChild(mus);
         var topMask = new egret.Shape();
         topMask.graphics.beginFill(0x000000, 0.5);
         topMask.graphics.drawRect(0, 0, stageW, 175);
--- src/Main.ts
+++ src/Main.ts
@@ -43,33 +43,32 @@
 
     private mouseMove(evt:egret.TouchEvent) {
             if( this._touchStatus ) {
-                 this.y = evt.stageY - this._distance.y;
-                
+                 this.y = evt.stageY - this._distance.y;            
             }            
-    }
-
-    public mouseUp(evt:egret.TouchEvent) {
-            this._touchStatus = false;
              if( this.y < -1136/3 ){
-                     egret.Tween.get( this ).to( {x:4/5*this.stageW,y:-1136}, 200, egret.Ease.sineIn )
-                     .wait(300).to({x:4/5*this.stageW,y:0}, 100, egret.Ease.sineIn);
-                     this.parent.addChildAt(this,1);
+                     egret.Tween.get( this ).to( {x:4/5*this.stageW,y:-1136}, 400, egret.Ease.sineIn )
+                     .call( ()=>{ this.parent.addChildAt(this,1);} ).to({x:4/5*this.stageW,y:0}, 100, egret.Ease.sineIn);
                      this.stage.removeEventListener(egret.TouchEvent.TOUCH_MOVE, this.mouseMove, this);
                  }
-                 else{ egret.Tween.get( this ).to( {x:4/5*this.stageW,y:0}, 200, egret.Ease.sineIn )
-                 .to( {x:4/5*this.stageW,y:20}, 100, egret.Ease.sineIn ).to( {x:4/5*this.stageW,y:0}, 100, egret.Ease.sineIn );
+                 else if(this.y<0){ egret.Tween.get( this ).to( {x:4/5*this.stageW,y:0}, 200, egret.Ease.sineIn )
+                 .to( {x:4/5*this.stageW,y:25}, 100, egret.Ease.sineIn ).to( {x:4/5*this.stageW,y:0}, 100, egret.Ease.sineIn );
                 
             }
                  if( this.y >1136/3 ){
-                     egret.Tween.get( this ).to( {x:4/5*this.stageW,y:-1136}, 200, egret.Ease.sineIn )
-                     .wait(300).to({x:4/5*this.stageW,y:0}, 100, egret.Ease.sineIn);
-                     this.parent.addChildAt(this,1);
+                     egret.Tween.get( this ).to( {x:4/5*this.stageW,y:1136}, 400, egret.Ease.sineIn )
+                     .call( ()=>{ this.parent.addChildAt(this,1);} ).to({x:4/5*this.stageW,y:0}, 100, egret.Ease.sineIn);           
                      this.stage.removeEventListener(egret.TouchEvent.TOUCH_MOVE, this.mouseMove, this);
-                 }else{ egret.Tween.get( this ).to( {x:4/5*this.stageW,y:0}, 200, egret.Ease.sineIn )
+                 }else if(this.y>0){ egret.Tween.get( this ).to( {x:4/5*this.stageW,y:0}, 200, egret.Ease.sineIn )
                  .to( {x:4/5*this.stageW,y:-25}, 100, egret.Ease.sineIn ).to( {x:4/5*this.stageW,y:0}, 100, egret.Ease.sineIn );}
    
 
 
+
+    }
+
+    public mouseUp(evt:egret.TouchEvent) {
+            this._touchStatus = false;
+            
             this.stage.removeEventListener(egret.TouchEvent.TOUCH_MOVE, this.mouseMove, this);
     }
 
@@ -77,7 +76,198 @@
     
   }
      
-     
+  class Music extends egret.DisplayObjectContainer {
+
+       private _sound: egret.Sound;
+       private _channel: egret.SoundChannel;
+       constructor() {
+       super();
+       this.addEventListener(egret.Event.ADDED_TO_STAGE, this.onAddToStage, this);
+    }
+
+    private onAddToStage(event: egret.Event) {
+        this.loadSound();
+    }
+
+    /*** 本示例关键代码段开始 ***/
+    //加载
+    private loadSound(): void {
+        var sound: egret.Sound = this._sound = new egret.Sound();;
+        //sound 加载完成监听
+        sound.addEventListener(egret.Event.COMPLETE, function (e: egret.Event) {
+            this.init();
+        }, this);
+
+        sound.load("resource/assets/ccnn.mp3");
+    }
+    //播放
+    private play():void {
+        //sound 播放会返回一个 SoundChannel 对象，暂停、音量等操作请控制此对象
+        this._channel = this._sound.play(this._pauseTime, 1);
+        this._channel.addEventListener(egret.Event.SOUND_COMPLETE, this.onComplete, this);
+        
+        this.addEventListener(egret.Event.ENTER_FRAME, this.onTimeUpdate, this);
+    }
+    //停止
+    private stop():void {
+        if (this._channel) {
+            this._channel.removeEventListener(egret.Event.SOUND_COMPLETE, this.onComplete, this);
+            this.removeEventListener(egret.Event.ENTER_FRAME, this.onTimeUpdate, this);
+            
+            this._channel.stop();
+            this._channel = null;
+        }
+    }
+    //播放完成
+    private onComplete(e:egret.Event):void {
+        console.log("播放完成");
+        this.stop();
+
+        this.setAllAbled(false);
+        
+        this.setProgress(0);
+    }
+    //更新进度
+    private onTimeUpdate(e:egret.Event):void {
+        var position:number = this._channel ? this._channel.position : 0;
+        
+        this.setProgress(position);
+    }
+    
+    private setProgress(position:number):void {
+        this._updateTxt.text = position.toFixed(1) + "/" +  this._sound.length.toFixed(1);
+        
+        var w:number= (position / this._sound.length) * 400;
+        this._bar.x = w + this.stage.stageWidth / 2 - 200;
+        
+        var mask:egret.Rectangle = <egret.Rectangle>this._progress.mask || new egret.Rectangle(0, 0, 0, 60);
+        mask.x = w;
+        mask.width = 400 - w;
+        this._progress.mask = mask;
+    }
+    
+    /*** 本示例关键代码段结束 ***/
+    
+    /** 以下为 UI 代码 **/
+    private _playTxt:egret.TextField;
+    private _pauseTxt:egret.TextField;
+    private _stopTxt:egret.TextField;
+    private _pauseTime: number = 30;
+    
+    private init(): void {
+        var rap:number = 180;
+        var rapH:number = 200;
+        
+        //play
+        var playTxt: egret.TextField = this._playTxt = new egret.TextField();
+        playTxt.text = "播放";
+        playTxt.size = 60;
+        playTxt.x = 80;
+        playTxt.y = 200 + rapH;
+        playTxt.touchEnabled = true;
+        playTxt.addEventListener(egret.TouchEvent.TOUCH_TAP, function () {
+            this.play();
+
+            this.setAllAbled(true);
+        }, this);
+        this.addChild(playTxt);
+
+        //stop
+        var stopTxt: egret.TextField = this._stopTxt = new egret.TextField();
+        stopTxt.text = "停止";
+        stopTxt.size = 60;
+        stopTxt.x = playTxt.x + rap * 1;
+        stopTxt.y = 200 + rapH;
+        stopTxt.touchEnabled = true;
+        stopTxt.addEventListener(egret.TouchEvent.TOUCH_TAP, function () {
+            if (this._channel) {
+                this._pauseTime = 0;
+                
+                this.stop();
+                
+                this.onTimeUpdate();
+            }
+
+            this.setAllAbled(false);
+
+        }, this);
+        this.addChild(stopTxt);
+
+        //pause 
+        var pauseTxt: egret.TextField = this._pauseTxt = new egret.TextField();
+        pauseTxt.text = "暂停";
+        pauseTxt.size = 60;
+        pauseTxt.x = playTxt.x + rap * 2;
+        pauseTxt.y = 200 + rapH;
+        pauseTxt.touchEnabled = true;
+        pauseTxt.addEventListener(egret.TouchEvent.TOUCH_TAP, function () {
+            if (this._channel) {
+                this._pauseTime = this._channel.position;
+                
+                this.stop();
+            }
+
+            this.setAllAbled(false);
+        }, this);
+        this.addChild(pauseTxt);
+
+        this.setAllAbled(false);
+        
+        var bg:egret.Shape = new egret.Shape();
+        this.addChild(bg);
+        bg.x = this.stage.stageWidth / 2 - 200;
+        bg.y = 100 - 5 + rapH;
+        bg.graphics.beginFill(0x999999);
+        bg.graphics.drawRoundRect(0, 0, 400, 10, 5, 5);
+        bg.graphics.endFill();
+        
+        this._progress = new egret.Shape();
+        this.addChild(this._progress);
+        this._progress.x = this.stage.stageWidth / 2 - 200;
+        this._progress.y = 100 - 5 + rapH;
+        this._progress.graphics.beginFill(0xffff00);
+        this._progress.graphics.drawRoundRect(0, 0, 400, 10, 5, 5);
+        this._progress.graphics.endFill();
+        
+        this._bar = new egret.Shape();
+        this.addChild(this._bar);
+        this._bar.x = this.stage.stageWidth / 2 - 200;
+        this._bar.y = 100 + rapH;
+        this._bar.graphics.beginFill(0xffff00);
+        this._bar.graphics.drawCircle(0, 0, 20);
+        this._bar.graphics.endFill();
+        
+        this._updateTxt = new egret.TextField();
+        this._updateTxt.text = 0 + "/" +  this._sound.length.toFixed(1);
+        this._updateTxt.width = 200;
+        this._updateTxt.size = 30;
+        this._updateTxt.x = this.stage.stageWidth / 2 - 100;
+        this._updateTxt.y = 50 + rapH;
+        this._updateTxt.textAlign = egret.HorizontalAlign.CENTER;
+        this.addChild(this._updateTxt);
+        
+    }
+    
+    private _bar:egret.Shape;
+    private _progress:egret.Shape;
+    private _updateTxt:egret.TextField;
+    
+    private setAllAbled(isPlaying:boolean):void {
+        this.setTextAbled(this._playTxt, !isPlaying);
+        this.setTextAbled(this._stopTxt, isPlaying);
+        this.setTextAbled(this._pauseTxt, isPlaying);
+    }
+    
+    private setTextAbled(text:egret.TextField, touchEnabled:boolean):void {
+        text.touchEnabled = touchEnabled;
+        if (touchEnabled) {
+            text.textColor = 0xffffff;
+        }
+        else {
+            text.textColor = 0x999999;
+        }
+    }
+  }
 
 
 
@@ -272,6 +462,9 @@ class Main extends egret.DisplayObjectContainer {
         p3.y=0;
         
 
+        var mus:Music =new Music;
+        this.addChild(mus);
+
         var topMask = new egret.Shape();
         topMask.graphics.beginFill(0x000000, 0.5);
         topMask.graphics.drawRect(0, 0, stageW, 175);
@@ -316,6 +509,8 @@ class Main extends egret.DisplayObjectContainer {
         textfield.x = 172;
         textfield.y = 135;
         this.textfield = textfield;
+
+
 
         //根据name关键字，异步获取一个json配置文件，name属性请参考resources/resource.json配置文件的内容。
         // Get asynchronously a json configuration file according to name keyword. As for the property of name please refer to the configuration file of resources/resource.json.
